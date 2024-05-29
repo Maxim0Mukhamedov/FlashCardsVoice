264ccde92caccee046b9f70e3c5b5c269cf30666
Ivan Golubev <g0lubew.iv@yandex.ru>
2024-05-30 01:15:35 +0300

    Mobile, middle, etc

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -7,23 +7,22 @@ export const TaskItem = (props) => {
   const { item, index, delTask } = props;
   return (
     <>
-    <details>
-        <summary class="summary-s">
-          <span class="summary-title">{item.title}</span>
-          <div class="summary-chevron-up">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" color="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+    <div className="question-card">
+      <details className="spoiler">
+          <summary class="summary-s">
+            <span class="summary-title">{item.title}</span>
+          </summary>
+          <div class="summary-content">
+            {item.ans}
           </div>
-        </summary>
-        <div class="summary-content">
-          {item.ans}
-        </div>
-    </details>
-    <input
-        className = "del-task-button"
-        type      = "button"
-        value     = "x"
-        onClick  = {(event) => delTask(item) }
-    />
+      </details>
+      <input
+          className = "del-task-button"
+          type      = "button"
+          value     = "x"
+          onClick  = {(event) => delTask(item) }
+      />
+    </div>
     </>
   )
 }
